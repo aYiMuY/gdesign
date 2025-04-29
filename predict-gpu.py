9742ce42d9e54c6c451076b8834c8511ef1be9e7
aYiMuY <107387515+aYiMuY@users.noreply.github.com>
2025-04-29 15:44:36 +0800

    Add files via upload

--- predict-gpu.py
+++ predict-gpu.py
@@ -90,10 +90,11 @@ pre_y[:, 1:] = -70 * pre_y[:, 1:]
 pre_y[:, 0] = 5 * pre_y[:, 0] + 5
 S11_batches = pre_y[:, 1:]
 Gain_batches = pre_y[:, 0]
-out_total = np.zeros((int(total_num), 4 + len(params[0])))
+out_total = np.zeros((int(total_num), 6 + len(params[0])))
 
 print(out_total.shape)
 #base = 811000
+cnt = 0
 for k,S11 in enumerate(S11_batches):#[base:base+1000]
     # Calculate the bandwidth
     S11 = [S11]
@@ -102,9 +103,11 @@ for k,S11 in enumerate(S11_batches):#[base:base+1000]
     Gain = [Gain]
     index = index.reshape(1,6)
     #print(index)
-    result = np.empty((0, 10))
+    result_2p3 = np.empty((0, 9))
     band = [np.where(line <= -10)[0] / 200 + 1.5 for line in S11]
     #print(S11)
+
+    #search for 2.3Ghz
     for i in range(1):
         a, b = [], []
         if band[i].any():
@@ -123,58 +126,104 @@ for k,S11 in enumerate(S11_batches):#[base:base+1000]
                         b.append(band[i][j])
                     else:
                         break
-        if not (2.45 in a):
+        if not (2.3 in a):
             a = []
-        if not (2.45 in b):
+        if not (2.3 in b):
+            b = []
+        # if not(a == []) and int((a[-1] - a[0]) * 1000) / h[i] >= 130:
+        if not (a == []):
+            # print('ID:', i + 1, ' Band:', a[0], 'GHz -', a[-1], 'GHz  Band Width:', int((a[-1] - a[0]) * 1000), 'MHz  H:', h[i], 'mm  B/H:', int(int((a[-1] - a[0]) * 1000) / h[i]))
+            result_2p3 = np.append(result_2p3, [
+                np.concatenate((index[i], np.array([a[0], a[-1], (a[-1] - a[0]) * 1000])),
+                               axis=0)], axis=0)
+            #print(1)
+        # if not (b == []) and int((b[-1] - b[0]) * 1000) / h[i] >= 130:
+        elif not (b == []):
+            # print('ID:', i + 1, ' Band:', b[0], 'GHz -', b[-1], 'GHz  Band Width:', int((b[-1] - b[0]) * 1000), 'MHz  H:', h[i], 'mm  B/H:', int(int((b[-1] - b[0]) * 1000) / h[i]))
+            result_2p3 = np.append(result_2p3, [
+                np.concatenate((index[i], np.array([b[0], b[-1], (b[-1] - b[0]) * 1000])),
+                               axis=0)], axis=0)
+            #print(2)
+        else:
+            result_2p3 = np.append(result_2p3,
+                               [np.concatenate((index[i], np.array([0, 0, 0])), axis=0)],
+                               axis=0)
+    result = np.empty((0,12))
+    #search for 3.3Ghz
+    for i in range(1):
+        a, b = [], []
+        if band[i].any():
+            #print('ok')
+            a = [band[i][0]]
+            for j in range(1, len(band[i])):
+                if round((band[i][j] - band[i][j - 1]) * 1000) / 1000 == 0.005:
+                    a.append(band[i][j])
+                else:
+                    break
+            if j < len(band[i]) - 1:
+                j0 = j
+                b = [band[i][j0]]
+                for j in range(j0 + 1, len(band[i])):
+                    if round((band[i][j] - band[i][j - 1]) * 1000) / 1000 == 0.005:
+                        b.append(band[i][j])
+                    else:
+                        break
+        if not (3.3 in a):
+            a = []
+        if not (3.3 in b):
             b = []
         # if not(a == []) and int((a[-1] - a[0]) * 1000) / h[i] >= 130:
         if not (a == []):
             # print('ID:', i + 1, ' Band:', a[0], 'GHz -', a[-1], 'GHz  Band Width:', int((a[-1] - a[0]) * 1000), 'MHz  H:', h[i], 'mm  B/H:', int(int((a[-1] - a[0]) * 1000) / h[i]))
             result = np.append(result, [
-                np.concatenate((index[i], np.array([a[0], a[-1], (a[-1] - a[0]) * 1000]),Gain),
+                np.concatenate((result_2p3[0], np.array([a[0], a[-1], (a[-1] - a[0]) * 1000])),
                                axis=0)], axis=0)
             #print(1)
         # if not (b == []) and int((b[-1] - b[0]) * 1000) / h[i] >= 130:
         elif not (b == []):
             # print('ID:', i + 1, ' Band:', b[0], 'GHz -', b[-1], 'GHz  Band Width:', int((b[-1] - b[0]) * 1000), 'MHz  H:', h[i], 'mm  B/H:', int(int((b[-1] - b[0]) * 1000) / h[i]))
             result = np.append(result, [
-                np.concatenate((index[i], np.array([b[0], b[-1], (b[-1] - b[0]) * 1000]),Gain),
+                np.concatenate((result_2p3[0], np.array([b[0], b[-1], (b[-1] - b[0]) * 1000])),
                                axis=0)], axis=0)
             #print(2)
         else:
             result = np.append(result,
-                               [np.concatenate((index[i], np.array([0, 0, 0]),Gain), axis=0)],
+                               [np.concatenate((result_2p3[0], np.array([0, 0, 0])), axis=0)],
                                axis=0)
 
+    if result[0,-1] >=50 and result[0,-4] >= 50:
+        out_total[cnt] = result
+        cnt += 1
+
     #Append the current result to the total output array
-    out_total[k] = result
-
-    # Check if the bandwidth is greater than 1000 MHz
-    if result[0, -2] > 1000:
-        # Append the result to the S11 output array
-        out_s11 = np.append(out_s11, result,
-                            axis=0)
-
-    # Check if the gain is greater than 9.5
-
-    if Gain[0] > 9.5:
-        # Append the result to the gain output array
-        out_gain = np.append(out_gain, result,
-                             axis=0)
-
-    # Check for specific conditions and append to the both output array
-    if h <= 7 and result[0, -2] > 700 and Gain[0] > 9.0:
-        out_both = np.append(out_both, result,
-                             axis=0)
-        #print('ook')
-        # Update the maximum gain and bandwidth encountered
-        if Gain[0] > Gain_max:
-                Gain_max = Gain[0]
-                arg_gain = result[0]
-
-        if result[0, -2] > BandWidth_max:
-                BandWidth_max = result[0, -2]
-                arg_bandwidth = result[0]
+    # out_total[k] = result
+    #
+    # # Check if the bandwidth is greater than 1000 MHz
+    # if result[0, -2] > 1000:
+    #     # Append the result to the S11 output array
+    #     out_s11 = np.append(out_s11, result,
+    #                         axis=0)
+    #
+    # # Check if the gain is greater than 9.5
+    #
+    # if Gain[0] > 9.5:
+    #     # Append the result to the gain output array
+    #     out_gain = np.append(out_gain, result,
+    #                          axis=0)
+    #
+    # # Check for specific conditions and append to the both output array
+    # if h <= 7 and result[0, -2] > 700 and Gain[0] > 9.0:
+    #     out_both = np.append(out_both, result,
+    #                          axis=0)
+    #     #print('ook')
+    #     # Update the maximum gain and bandwidth encountered
+    #     if Gain[0] > Gain_max:
+    #             Gain_max = Gain[0]
+    #             arg_gain = result[0]
+    #
+    #     if result[0, -2] > BandWidth_max:
+    #             BandWidth_max = result[0, -2]
+    #             arg_bandwidth = result[0]
 
     if k % delta == 0:
                             print("%d / %d is Done!" % (k, total_num))
@@ -184,16 +233,17 @@ for k,S11 in enumerate(S11_batches):#[base:base+1000]
 
 
 #Save the output data to text files
-np.savetxt(S11_Save_path, out_s11, fmt='%.3f')
-np.savetxt(Gain_Save_path, out_gain, fmt='%.3f')
-np.savetxt(Both_Save_path, out_both, fmt='%.3f')
-
-# Append the maximum gain and bandwidth results to the out_max array
-out_max = np.append(out_max, [arg_gain], axis=0)
-out_max = np.append(out_max, [arg_bandwidth], axis=0)
-
-# Save the out_max array
-np.savetxt(MAX_Save_path, out_max, fmt='%.3f')
+# np.savetxt(S11_Save_path, out_s11, fmt='%.3f')
+# np.savetxt(Gain_Save_path, out_gain, fmt='%.3f')
+# np.savetxt(Both_Save_path, out_both, fmt='%.3f')
+#
+# # Append the maximum gain and bandwidth results to the out_max array
+# out_max = np.append(out_max, [arg_gain], axis=0)
+# out_max = np.append(out_max, [arg_bandwidth], axis=0)
+#
+# # Save the out_max array
+# np.savetxt(MAX_Save_path, out_max, fmt='%.3f')
+out_total = out_total[:cnt]
 
 # Save the total output array
 np.savetxt(Total_Save_path, out_total, fmt='%.3f')
