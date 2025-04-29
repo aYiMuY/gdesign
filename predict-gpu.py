938a3672f9c9964c5c4f15baa8b1f3ff7ed6dc87
aYiMuY <107387515+aYiMuY@users.noreply.github.com>
2025-04-29 14:34:27 +0800

    Add files via upload

---
+++ predict-gpu.py
@@ -0,0 +1,284 @@
+# from __future__ import print_function
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from keras.layers import Input, Dense, Lambda
+from keras.models import Model, Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import SGD
+from keras.models import Model
+from keras import backend as K
+from keras.models import load_model
+import tensorflow as tf
+import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution()
+import tensorflow.python.util.deprecation as deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False
+from keras.layers import ReLU
+from keras.callbacks import TensorBoard
+from keras.layers import Activation
+from keras.utils.generic_utils import get_custom_objects
+from keras import losses
+
+from keras.callbacks import EarlyStopping
+from matplotlib import pyplot as plt
+
+from train import model, input_train, index_array
+
+
+
+# Step size for parameter iteration
+step = 0.5
+
+# Arrays to store the output data
+out_s11 = np.empty((0, 10))
+out_gain = np.empty((0, 10))
+out_both = np.empty((0, 10))
+out_max = np.empty((0, 10))
+out_total = np.empty((0, 10))
+
+# File paths for saving the output data
+S11_Save_path = "Output_Bandwidth1000+.txt"
+Gain_Save_path = "Output_Gain9.5+.txt"
+Both_Save_path = "Output_Both.txt"
+MAX_Save_path = "Output_Max.txt"
+Total_Save_path = "Output_Total.txt"
+
+# Calculate the total number of iterations based on the parameter ranges and step size
+
+
+
+# Initialize counters and variables
+num = 0
+delta = 1000  # Interval for progress updates
+Gain_max = 0  # Maximum gain encountered
+BandWidth_max = -1  # Maximum bandwidth encountered
+
+total_num = (5 / 1 + 1) * (12 / step + 1) * (8 / step + 1) * (4 / step + 1) * (5 / step + 1) * (4 / step + 1)
+print("Total Number:", total_num)
+
+# Iterate over the parameter space
+h_range = np.arange(5,11,1)
+Scale_X_range = np.arange(69.0,81.5,0.5)
+Scale_Y_range = np.arange(37.0,45.5,0.5)
+Offset_y_range = np.arange(-8.0,-3.5,0.5)
+Scale_Slot_range = np.arange(8.0,13.5,0.5)
+Uw3_range = np.arange(9.0,13.5,0.5)
+
+# total_num = 1
+# print("Total Number:", total_num)
+#
+# h_range = np.arange(7,8,1)
+# Scale_X_range = np.arange(71.0,71.5,0.5)
+# Scale_Y_range = np.arange(40.0,40.5,0.5)
+# Offset_y_range = np.arange(-6.5,-6.0,0.5)
+# Scale_Slot_range = np.arange(8.0,8.5,0.5)
+# Uw3_range = np.arange(9.0,9.5,0.5)
+
+grids = np.meshgrid(h_range,Scale_X_range,Scale_Y_range,Offset_y_range,Scale_Slot_range,Uw3_range)
+params = np.vstack([g.ravel() for g in grids]).T
+normalized_params = (params - index_array[0]) / (index_array[1] - index_array[0])
+pre_y = model.predict(normalized_params, batch_size=1024)
+print(len(pre_y),total_num,len(params))
+
+pre_y[:, 1:] = -70 * pre_y[:, 1:]
+pre_y[:, 0] = 5 * pre_y[:, 0] + 5
+S11_batches = pre_y[:, 1:]
+Gain_batches = pre_y[:, 0]
+out_total = np.zeros((int(total_num), 4 + len(params[0])))
+
+print(out_total.shape)
+#base = 811000
+for k,S11 in enumerate(S11_batches):#[base:base+1000]
+    # Calculate the bandwidth
+    S11 = [S11]
+    index , Gain = params[k] , Gain_batches[k]
+    h = index[0]
+    Gain = [Gain]
+    index = index.reshape(1,6)
+    #print(index)
+    result = np.empty((0, 10))
+    band = [np.where(line <= -10)[0] / 200 + 1.5 for line in S11]
+    #print(S11)
+    for i in range(1):
+        a, b = [], []
+        if band[i].any():
+            #print('ok')
+            a = [band[i][0]]
+            for j in range(1, len(band[i])):
+                if round((band[i][j] - band[i][j - 1]) * 1000) / 1000 == 0.005:
+                    a.append(band[i][j])
+                else:
+                    break
+            if j < len(band[i]) - 1:
+                j0 = j
+                b = [band[i][j0]]
+                for j in range(j0 + 1, len(band[i])):
+                    if round((band[i][j] - band[i][j - 1]) * 1000) / 1000 == 0.005:
+                        b.append(band[i][j])
+                    else:
+                        break
+        if not (2.45 in a):
+            a = []
+        if not (2.45 in b):
+            b = []
+        # if not(a == []) and int((a[-1] - a[0]) * 1000) / h[i] >= 130:
+        if not (a == []):
+            # print('ID:', i + 1, ' Band:', a[0], 'GHz -', a[-1], 'GHz  Band Width:', int((a[-1] - a[0]) * 1000), 'MHz  H:', h[i], 'mm  B/H:', int(int((a[-1] - a[0]) * 1000) / h[i]))
+            result = np.append(result, [
+                np.concatenate((index[i], np.array([a[0], a[-1], (a[-1] - a[0]) * 1000]),Gain),
+                               axis=0)], axis=0)
+            #print(1)
+        # if not (b == []) and int((b[-1] - b[0]) * 1000) / h[i] >= 130:
+        elif not (b == []):
+            # print('ID:', i + 1, ' Band:', b[0], 'GHz -', b[-1], 'GHz  Band Width:', int((b[-1] - b[0]) * 1000), 'MHz  H:', h[i], 'mm  B/H:', int(int((b[-1] - b[0]) * 1000) / h[i]))
+            result = np.append(result, [
+                np.concatenate((index[i], np.array([b[0], b[-1], (b[-1] - b[0]) * 1000]),Gain),
+                               axis=0)], axis=0)
+            #print(2)
+        else:
+            result = np.append(result,
+                               [np.concatenate((index[i], np.array([0, 0, 0]),Gain), axis=0)],
+                               axis=0)
+
+    #Append the current result to the total output array
+    out_total[k] = result
+
+    # Check if the bandwidth is greater than 1000 MHz
+    if result[0, -2] > 1000:
+        # Append the result to the S11 output array
+        out_s11 = np.append(out_s11, result,
+                            axis=0)
+
+    # Check if the gain is greater than 9.5
+
+    if Gain[0] > 9.5:
+        # Append the result to the gain output array
+        out_gain = np.append(out_gain, result,
+                             axis=0)
+
+    # Check for specific conditions and append to the both output array
+    if h <= 7 and result[0, -2] > 700 and Gain[0] > 9.0:
+        out_both = np.append(out_both, result,
+                             axis=0)
+        #print('ook')
+        # Update the maximum gain and bandwidth encountered
+        if Gain[0] > Gain_max:
+                Gain_max = Gain[0]
+                arg_gain = result[0]
+
+        if result[0, -2] > BandWidth_max:
+                BandWidth_max = result[0, -2]
+                arg_bandwidth = result[0]
+
+    if k % delta == 0:
+                            print("%d / %d is Done!" % (k, total_num))
+                            print("Current Parameters:", index[0])
+                            print()
+#out_total = out_total[:1000]
+
+
+#Save the output data to text files
+np.savetxt(S11_Save_path, out_s11, fmt='%.3f')
+np.savetxt(Gain_Save_path, out_gain, fmt='%.3f')
+np.savetxt(Both_Save_path, out_both, fmt='%.3f')
+
+# Append the maximum gain and bandwidth results to the out_max array
+out_max = np.append(out_max, [arg_gain], axis=0)
+out_max = np.append(out_max, [arg_bandwidth], axis=0)
+
+# Save the out_max array
+np.savetxt(MAX_Save_path, out_max, fmt='%.3f')
+
+# Save the total output array
+np.savetxt(Total_Save_path, out_total, fmt='%.3f')
+
+
+
+# Update the iteration counter
+
+
+# # Calculate the bandwidth
+# #
+# index = np.array([7.0,71.0,40.0,-6.5,8.0,9.0]).reshape([1,6])
+# index_norm = (index - index_array[0, :]) / (index_array[1, :] - index_array[0, :])
+# pre_y = model.predict(index_norm)
+# pre_y[:, 1:] = -70 * pre_y[:, 1:]
+# pre_y[:, 0] = 5 * pre_y[:, 0] + 5
+# S11 = pre_y[:,1:]
+# Gain = pre_y[:, 0]
+# #print(S11)
+# result = np.empty((0, 10))
+# band = [np.where(line <= -10)[0] / 200 + 1.5 for line in S11]
+# for i in range(1):
+#     a, b = [], []
+#     if band[i].any():
+#         a = [band[i][0]]
+#         for j in range(1, len(band[i])):
+#             if round((band[i][j] - band[i][j - 1]) * 1000) / 1000 == 0.005:
+#                 a.append(band[i][j])
+#             else:
+#                 break
+#         if j < len(band[i]) - 1:
+#             j0 = j
+#             b = [band[i][j0]]
+#             for j in range(j0 + 1, len(band[i])):
+#                 if round((band[i][j] - band[i][j - 1]) * 1000) / 1000 == 0.005:
+#                     b.append(band[i][j])
+#                 else:
+#                     break
+#     if not (2.45 in a):
+#         a = []
+#     if not (2.45 in b):
+#         b = []
+#     # if not(a == []) and int((a[-1] - a[0]) * 1000) / h[i] >= 130:
+#     if not (a == []):
+#         # print('ID:', i + 1, ' Band:', a[0], 'GHz -', a[-1], 'GHz  Band Width:', int((a[-1] - a[0]) * 1000), 'MHz  H:', h[i], 'mm  B/H:', int(int((a[-1] - a[0]) * 1000) / h[i]))
+#         result = np.append(result, [
+#             np.concatenate((index[i], np.array([a[0], a[-1], (a[-1] - a[0]) * 1000]),Gain),
+#                            axis=0)], axis=0)
+#     # if not (b == []) and int((b[-1] - b[0]) * 1000) / h[i] >= 130:
+#     elif not (b == []):
+#         # print('ID:', i + 1, ' Band:', b[0], 'GHz -', b[-1], 'GHz  Band Width:', int((b[-1] - b[0]) * 1000), 'MHz  H:', h[i], 'mm  B/H:', int(int((b[-1] - b[0]) * 1000) / h[i]))
+#         result = np.append(result, [
+#             np.concatenate((index[i], np.array([b[0], b[-1], (b[-1] - b[0]) * 1000]),Gain),
+#                            axis=0)], axis=0)
+#     else:
+#         result = np.append(result,
+#                            [np.concatenate((index[i], np.array([0, 0, 0]),Gain), axis=0)],
+#                           axis=0)
+#
+#     #Append the current result to the total output array
+#
+# h = index[0][0]
+#
+# # Check if the bandwidth is greater than 1000 MHz
+# print( result[0, -2]  ,result[0, -2] > 1000)
+#
+#
+# # Check if the gain is greater than 9.5
+#
+# print(Gain[0],Gain[0] > 9.5)
+#     # Append the result to the gain output array
+#
+#
+# # Check for specific conditions and append to the both output array
+# print( h, h <= 7 and result[0, -2] > 700 and Gain[0] > 9.0)
+#
+# if Gain[0] > Gain_max:
+#         Gain_max = Gain[0]
+#         arg_gain = result[0]
+#
+# if result[0, -2] > BandWidth_max:
+#         BandWidth_max = result[0, -2]
+#         arg_bandwidth = result[0]
+#
+# print(result)
+# #
+
+
---
+++ train.py
@@ -0,0 +1,167 @@
+import numpy as np
+from tensorflow import keras
+from scipy.stats import norm
+from keras.layers import Input, Dense, Lambda
+from keras.models import Model, Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import SGD
+from keras.models import Model
+from keras import backend as K
+from keras.models import load_model
+from keras.layers import ReLU
+from keras.callbacks import TensorBoard
+from keras.layers import Activation
+from keras.utils.generic_utils import get_custom_objects
+from keras import losses
+from keras.callbacks import EarlyStopping
+from matplotlib import pyplot as plt
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+# Load the dataset from a text file
+data = np.loadtxt(r'train_data.txt')
+
+# Shuffle the data randomly
+np.random.seed(0)
+np.random.shuffle(data)
+
+# Separate features and labels
+index = data[:, :6]  # Indices for the parameters
+gain = data[:, 6]  # Gain values
+S11 = data[:, 7:]  # S11 magnitude data
+# Normalize the S11 data
+[row, column] = np.shape(S11)
+S11 = -S11  # Invert the S11 values
+S11[S11 < 0] = 0  # Set negative values to zero
+max_S11 = 70
+min_S11 = 0
+S11_norm = S11 / max_S11  # Normalize S11
+
+# Normalize the gain data
+max_gain = 10
+min_gain = 5
+gain_norm = (gain - min_gain) / (max_gain - min_gain)
+
+# Normalize the index data
+index1_h_min = 6 - 1
+index1_h_max = 9 + 1
+index2_Scale_X_min = 70 - 1
+index2_Scale_X_max = 80 + 1
+index3_Scale_Y_min = 38 - 1
+index3_Scale_Y_max = 44 + 1
+index4_Offset_y_min = -7 - 1
+index4_Offset_y_max = -5 + 1
+index5_Scale_Slot_m10_min = 9 - 1
+index5_Scale_Slot_m10_max = 12 + 1
+index6_Uw3_min = 10 - 1
+index6_Uw3_max = 12 + 1
+index_array = np.array([[index1_h_min, index2_Scale_X_min, index3_Scale_Y_min, index4_Offset_y_min, index5_Scale_Slot_m10_min, index6_Uw3_min], [index1_h_max, index2_Scale_X_max, index3_Scale_Y_max, index4_Offset_y_max, index5_Scale_Slot_m10_max, index6_Uw3_max]])
+index_norm = (index - index_array[0, :]) / (index_array[1, :] - index_array[0, :])
+
+# Split the data into training and testing sets
+train_number = 3000
+train_S11_norm = S11_norm[:train_number, :]
+train_gain_norm = gain_norm[:train_number]
+train_index_norm = index_norm[:train_number, :]
+
+test_S11_norm = S11_norm[train_number:, :]
+test_gain_norm = gain_norm[train_number:]
+test_index_norm = index_norm[train_number:, :]
+
+input_train = train_index_norm  # (3000, 6)
+output1_train = train_gain_norm
+output1_train = output1_train.reshape(train_number, 1)  # (3000, 1)
+output2_train = train_S11_norm  # (3000, 401)
+output_train = np.hstack((output1_train, output2_train))  # (3000, 402)
+
+input_test = test_index_norm  # (3000, 6)
+output1_test = test_gain_norm
+output1_test = output1_test.reshape(3456 - train_number, 1)  # (456, 1)
+output2_test = test_S11_norm  # (456, 401)
+output_test = np.hstack((output1_test, output2_test))  # (456, 402)
+
+# Define and compile the deep learning model
+model = Sequential()
+model.add(keras.layers.Dense(8, input_dim=input_train.shape[1], activation="relu"))
+model.add(keras.layers.Dense(16, activation="relu"))
+model.add(keras.layers.Dense(32, activation="relu"))
+model.add(keras.layers.Dense(64, activation="relu"))
+model.add(keras.layers.Dense(128, activation="relu"))
+model.add(keras.layers.Dense(256, activation="relu"))
+model.add(keras.layers.Dense(512, activation="relu"))
+model.add(keras.layers.Dense(402, activation="sigmoid"))  # Output layer with 402 neurons (1 for gain and 401 for S11)
+model.summary()
+
+# Define the path and callback for saving the model during training
+filepath = "saved-model-{epoch:02d}.h5"
+checkpoint = keras.callbacks.ModelCheckpoint(filepath=filepath, monitor="val_loss", mode="min", save_weights_only=True, save_best_only=False, verbose=1, period=1000)
+
+# Compile the model with mean squared error loss and Adam optimizer
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+######train
+# # Train the model with the training data
+# model_fit = model.fit(input_train, output_train, batch_size=512,
+#                       epochs=5000, verbose=0,
+#                       validation_split=0.2,
+#                       callbacks=[checkpoint])
+
+# Visualize the training and validation loss
+# plt.figure()
+# plt.plot(model_fit.history["loss"])
+# plt.plot(model_fit.history["val_loss"])
+# plt.xlabel("iters")
+# plt.ylabel("loss")
+# plt.show()
+
+# Load the weights of the trained model
+model.load_weights("saved-model-5000.h5")
+
+# Predict on the training set and calculate the mean absolute error and mean squared error
+pre_y = model.predict(input_train)
+print("mean absolute error:", keras.metrics.mean_absolute_error(output_train, pre_y))
+print("mean squared error:", keras.metrics.mean_squared_error(output_train, pre_y))
+
+
+######plot show
+# # Plot S11 for a few random samples from the training set
+# for i in range(3):
+#     index = np.random.randint(0, 2999)
+#     plt.plot(np.arange(401), output_train[index, 1:])
+#     plt.plot(np.arange(401), pre_y[index, 1:])
+#     plt.title("Train%d, Index%d" % (i + 1, index))
+#     plt.show()
+
+# # Plot Gain for the training set
+# plt.scatter(np.arange(3000), output_train[:, 0], marker='o', s=10, c=None, edgecolors='r')
+# plt.scatter(np.arange(3000), pre_y[:, 0], marker='x', s=10, c='black')
+# plt.title("Training Gain")
+# plt.show()
+
+# Predict on the test set and calculate the mean absolute error and mean squared error
+pre_y = model.predict(input_test)
+print("mean absolute error:", keras.metrics.mean_absolute_error(output_test, pre_y))
+print("mean squared error:", keras.metrics.mean_squared_error(output_test, pre_y))
+
+# Reverse normalization for S11 and Gain for the test set and predictions
+output_test[:, 1:] = -70 * output_test[:, 1:]
+output_test[:, 0] = 5 * output_test[:, 0] + 5
+pre_y[:, 1:] = -70 * pre_y[:, 1:]
+pre_y[:, 0] = 5 * pre_y[:, 0] + 5
+print(input_test)
+
+
+#####plot show
+# Plot S11 for a few random samples from the test set
+# for i in range(3):
+#     index = np.random.randint(0, 455)
+#     plt.plot(np.arange(401), output_test[index, 1:], 'black')
+#     plt.plot(np.arange(401), pre_y[index, 1:])
+#     plt.title("Test%d, Index%d" % (i + 1, index))
+#     plt.show()
+#
+# # Plot Gain for the test set
+# plt.scatter(np.arange(100), output_test[:100, 0], marker='o', s=40, c='White', edgecolors='r')
+# plt.scatter(np.arange(100), pre_y[:100, 0], marker='x', s=40, c='black')
+# plt.title("Testing Gain")
+# plt.show()
